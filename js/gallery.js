ae6471aa75c262434dc86b3ecaf7f094e5d96ef3
Angela Hwang <hwangangela99@hotmail.com>
2015-11-30 11:27:33 -0600

    3d gallery add pictures

--- js/gallery.js
+++ js/gallery.js
@@ -18,13 +18,37 @@ $(document).ready(function() {
     $(this).lightGallery({
       dynamic: true,
       dynamicEl: [{
-        'src': 'lightGallery-master/demo/img/2.jpg',
-        'thumb': "lightGallery-master/demo/img/thumb-2.jpg",
-        'subHtml': '<h4>Title</h4><p>Description</p>'
+        'src': 'img/menu/3d/bowl1.jpg',
+        'thumb': "img/menu/3d/bowl1.jpg",
+        'subHtml': '<h4>Bowl</h4>'
       }, {
-        'src': 'lightGallery-master/demo/img/1.jpg',
-        'thumb': 'lightGallery-master/demo/img/thumb-1.jpg',
-        'subHtml': '<h4>Title</h4><p>Description</p>'
+        'src': 'img/menu/3d/enchilada1.jpg',
+        'thumb': 'img/menu/3d/enchilada1.jpg',
+        'subHtml': '<h4>Enchiladas</h4>'
+      }, {
+        'src': 'img/menu/3d/fox1.jpg',
+        'thumb': 'img/menu/3d/fox1.jpg',
+        'subHtml': '<h4>Nine-tailed Fox</h4>'
+      }, {
+        'src': 'img/menu/3d/lotus2.jpg',
+        'thumb': 'img/menu/3d/lotus2.jpg',
+        'subHtml': '<h4>Floating Lotus</h4>'
+      }, {
+        'src': 'img/menu/3d/native1.jpg',
+        'thumb': 'img/menu/3d/native1.jpg',
+        'subHtml': '<h4>Native American Art</h4>'
+      }, {
+        'src': 'img/menu/3d/plate1.jpg',
+        'thumb': 'img/menu/3d/plate1.jpg',
+        'subHtml': '<h4>Plate</h4>'
+      }, {
+        'src': 'img/menu/3d/snake1.jpg',
+        'thumb': 'img/menu/3d/snake1.jpg',
+        'subHtml': '<h4>Boa Constrictor</h4>'
+      }, {
+        'src': 'img/menu/3d/turtle1.jpg',
+        'thumb': 'img/menu/3d/turtle1.jpg',
+        'subHtml': '<h4>Turtle</h4>'
       }]
     })
   });
