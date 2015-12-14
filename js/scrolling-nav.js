e50fa7e21540790037c6448eae67f5ba23782aaf
Angela Hwang <hwangangela99@hotmail.com>
2015-12-13 20:36:46 -0600

    gal item one at a time

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,16 +1,21 @@
 //jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
+/*    if($(".navbar").offset().top > 200) {
+      $('#carrot').animate({opacity:1});
+    } else {
+      $('#carrot').animate({opacity:0});
+    } */
+
     if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse", function() {
-          $("#carrot").animate({opacity:1}, 'slow');
-        });
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+        $('#carrot').addClass('visible');
 
     } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse", function() {
-          $("#carrot").animate({opacity:0}, 'slow');
-        });
-
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $('#carrot').removeClass('visible');
     }
+
+
 });
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
